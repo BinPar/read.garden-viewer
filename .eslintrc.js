ca3ec625f8cfc82f4b6543260867d156a07f912a
drnachio <nacho@binpar.com>
2021-04-29 08:58:02 +0200

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['airbnb-typescript', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  entry: './dist/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'web/js'),
+    filename: 'read.garden-viewer.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
+};
