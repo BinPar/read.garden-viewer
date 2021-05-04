6fb3ee67d660e8ef351f69a301c71075e9b17156
Ignacio Ferro Picón <nacho@binpar.com>
2021-05-04 16:21:15 +0200

    works like charm

--- tests/general/viewer.ts
+++ tests/general/viewer.ts
@@ -1,12 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import 'expect-puppeteer';
 
 describe(
   'General',
   () => {    
     it('should load without error', async () => {
-      await page.goto('http://localhost:3000/');
-      const text = await page.evaluate(() => document.body.textContent);
-      expect(text).toContain('Viewer');
+      await page.goto('http://localhost:3000/');      
+      const title = await page.evaluate(() => document.title)      
+      expect(title).toContain('Viewer');     
     });
   },
 );
