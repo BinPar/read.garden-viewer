e9c52a265c3a3027fd6ec3c3001c21e8f4a76893
Ignacio Ferro Picón <nacho@binpar.com>
2021-05-04 17:49:59 +0200

    doc

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,8 @@ module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
     product: process.env.FIREFOX === 'true' ? 'firefox' : 'chrome',
+    // true to display the browser console output in the terminal
+    // very useful for debugging
     dumpio: true,
   },
 }
