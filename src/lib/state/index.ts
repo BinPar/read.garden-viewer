6d5d04f8178028c94f7e10e48be1ee21749b0cf3
Miguel Rodríguez <miguel@binpar.com>
2021-05-04 20:15:38 +0200

    simplified dispatch until further notice

--- src/lib/state/index.ts
+++ src/lib/state/index.ts
@@ -14,6 +14,6 @@ export const getState = (): StateWithConfig => ({
   config: getConfig(),
 });
 
-export const setState = (newState: Partial<State>): void => {
+export const updateState = (newState: Partial<State>): void => {
   state = merge(state, newState);
 };
