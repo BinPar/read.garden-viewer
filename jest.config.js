d584e272ec9f84e388f47d2067e18586f5514737
Ignacio Ferro Picón <nacho@binpar.com>
2021-05-04 12:13:39 +0200

    fix

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   globalSetup: './jest/setup.js',
   globalTeardown: './jest/teardown.js',
   testEnvironment: './jest/puppeteer_environment.js',
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
