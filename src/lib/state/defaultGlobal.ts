5e0af8967e6a7827516efbd1110e07021a9c9368
Miguel Rodríguez <miguel@binpar.com>
2021-05-05 17:32:38 +0200

    new props added to config model, added to initialize method and documented

--- src/lib/state/defaultGlobal.ts
+++ src/lib/state/defaultGlobal.ts
@@ -2,6 +2,8 @@ import { DefaultGlobalState } from '../../model/state';
 
 const defaultGlobal: DefaultGlobalState = {
   scale: 1,
+  debugViewerSafeArea: false,
+  basicDOMElementsCreated: false,
 };
 
 export default defaultGlobal;
