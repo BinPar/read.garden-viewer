0c8fc894d00fc487ca548d111e1cba1ab54b3021
Ignacio Ferro Picón <nacho@binpar.com>
2021-05-05 20:52:53 +0200

    fix

--- src/lib/actions/appendNewContent.ts
+++ src/lib/actions/appendNewContent.ts
@@ -2,7 +2,7 @@ import { ActionDispatcher } from '../../model/actions/actionDispatcher';
 import { AppendNewContent } from '../../model/actions/global';
 
 const appendNewContent: ActionDispatcher<AppendNewContent> = async (action) => {
-   console.log(action.htmlContent);
+   console.log(action.cssURL);
    return {
 
    };
