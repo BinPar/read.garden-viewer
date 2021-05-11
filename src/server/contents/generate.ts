a8821a89904a7e4e60b2522f9da07928f50e2b3b
Ignacio Ferro Picón <nacho@binpar.com>
2021-05-11 19:37:28 +0200

    Parallel download

--- src/server/contents/generate.ts
+++ src/server/contents/generate.ts
@@ -1,11 +1,10 @@
+/* eslint-disable no-await-in-loop */
 import * as path from 'path';
 import { ensureDir } from '@a2r/fs';
 import log from 'loglevel';
-
 import books from './data/books';
 import download from './download';
 import oneFileToPath from './oneFileToPath';
-
 import { cdnDomain } from '../config';
 
 /**
@@ -17,22 +16,20 @@ const generate = async (cdn?: string, destDir?: string): Promise<void> => {
   log.setLevel('info');
   const booksPath = destDir || path.join(__dirname, '../../../web/books');
   await ensureDir(booksPath);
-  const booksDownloads = books.map(
-    async (book): Promise<void> => {
-      const remoteURI = `https://${cdnDomain}/books/${book}/${book}.bp`;
-      const bookPath = `${booksPath}/${book}`;
-      const bookFile = `${booksPath}/${book}/${book}.bp`;
-      try {
-        await ensureDir(bookPath);      
-        await download(remoteURI, bookFile);
-        await oneFileToPath(bookFile, bookPath);
-      } catch (ex) {
-        log.error(`Error downloading ${remoteURI}:`);
-        log.error(ex);
-      }
-    },
-  );
-  Promise.all(booksDownloads);
+  for(let i=0; i< books.length;i++) {
+    const book = books[i];
+    const remoteURI = `https://${cdnDomain}/books/${book}/${book}.bp`;
+    const bookPath = `${booksPath}/${book}`;
+    const bookFile = `${booksPath}/${book}/${book}.bp`;
+    try {
+      await ensureDir(bookPath);      
+      await download(remoteURI, bookFile);
+      await oneFileToPath(bookFile, bookPath);
+    } catch (ex) {
+      log.error(`Error downloading ${remoteURI}:`);
+      log.error(ex);
+    }
+  }
 };
 
 export default generate;
