3bc81f5c2726c936acb36dce984e9f93ee15c142
Miguel Rodríguez <miguel@binpar.com>
2021-05-18 12:24:35 +0200

    small fixes

--- src/utils/setupGlobalEvents.ts
+++ src/utils/setupGlobalEvents.ts
@@ -1,10 +1,7 @@
-import log from 'loglevel';
 import { getState, updateState } from '../lib/state';
 import { Resize } from '../model/actions/global';
 import { DispatchAPIAction } from '../model/apiInterface';
-import { LayoutTypes } from '../model/state';
-import getCurrentPageInViewport from './getCurrentPageInViewport';
-import loadContentsInBackground from './loadContentsInBackground';
+import checkCurrentPage from './checkCurrentPage';
 
 let onResize: () => void;
 let onScroll: () => void;
@@ -23,18 +20,7 @@ const setupGlobalEvents = (dispatcher: DispatchAPIAction): void => {
   window.addEventListener('resize', onResize);
 
   onScroll = (): void => {
-    const label = getCurrentPageInViewport();
-    if (label) {
-      if (state.layout === LayoutTypes.Fixed) {
-        const content = state.contentsByLabel.get(label);
-        if (content && content.slug && state.contentSlug !== content.slug) {
-          log.info('Page changed by scroll', content.slug);
-          updateState({ currentContentIndex: content.order });
-          loadContentsInBackground();
-        }
-      }
-      // TODO: Flow behavior
-    }
+    checkCurrentPage();
   };
 
   window.addEventListener('scroll', onScroll);
