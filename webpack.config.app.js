172ea5b2cd53833b9cbb1baa3a6db2ae417b6990
Marcos González Andrés <marcos@binpar.com>
2021-07-01 11:44:38 +0200

    app settings

--- webpack.config.app.js
+++ webpack.config.app.js
@@ -4,15 +4,6 @@ const path = require('path');
 module.exports = {
   entry: './dist/index.js',
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: { loader: 'babel-loader' }
-      }
-    ]
-  },
   output: {
     path: path.resolve(__dirname, '../read.garden-mobile/sdk-viewer/js/'),
     filename: 'read.garden-viewer.js',
