1b8cfa18544e923ab6f38361401852332f21bbde
Miguel Rodríguez <miguel@binpar.com>
2021-09-20 19:56:24 +0200

    selection in fixed improved CU-kz6rfn

--- src/lib/animation/getRangeFromPoint.ts
+++ src/lib/animation/getRangeFromPoint.ts
@@ -4,7 +4,7 @@ import getSelection from './getSelection';
 /**
  * This is crazy... the selection from point is not standard jet
  * it took some time to figure out how to do it in the different
- * browsers (old scroll JS) but I think that it covers every browser 
+ * browsers (old scroll JS) but I think that it covers every browser
  * (at least for the Bi-Books needs), if anyone detects any improvement
  * for any browser please do a pool request
  * @param {SyntheticEvent} evt - Mouse event to do the selection
@@ -22,24 +22,20 @@ const getRangeFromPoint = (evt: SyntheticEvent, extendToWord = false): Range => 
   if (body.createTextRange) {
     range = body.createTextRange();
     range.moveToPoint(x, y);
-  } else if (typeof document.createRange !== "undefined") {
-    // Try Mozilla's rangeOffset and rangeParent properties, which are exactly what we want
-    if (typeof evt.rangeParent !== "undefined") {
+  } else if (typeof document.createRange !== 'undefined') {
+    if (typeof evt.rangeParent !== 'undefined') {
+      // Try Mozilla's rangeOffset and rangeParent properties, which are exactly what we want
       range = document.createRange();
       range.setStart(evt.rangeParent, evt.rangeOffset);
       range.collapse(true);
-    }
-
-    // Try the standards-based way next
-    else if (document.caretPositionFromPoint) {
-      const pos = document.caretPositionFromPoint(x, y) as any;
+    } else if ((document as any).caretPositionFromPoint) {
+      // Try the standards-based way next
+      const pos = (document as any).caretPositionFromPoint(x, y) as any;
       range = document.createRange();
       range.setStart(pos.offsetNode, pos.offset);
       range.collapse(true);
-    }
-
-    // Next, the WebKit way
-    else if (document.caretRangeFromPoint) {
+    } else if (document.caretRangeFromPoint) {
+      // Next, the WebKit way
       range = document.caretRangeFromPoint(x, y);
     }
   }
@@ -54,6 +50,6 @@ const getRangeFromPoint = (evt: SyntheticEvent, extendToWord = false): Range => 
     }
   }
   return range as Range;
-}
+};
 
 export default getRangeFromPoint;
--- src/lib/animation/getWordSelection.ts
+++ src/lib/animation/getWordSelection.ts
@@ -17,16 +17,16 @@ const getWordSelection = (
     if (ev.target) {
       const { contentPlaceholderNode } = state;
       const target = ev.target as HTMLElement;
+      const targetStyle = window.getComputedStyle(target);
       if (
         force ||
-        (contentPlaceholderNode?.contains(target) &&
-          window.getComputedStyle(target).userSelect !== 'none')
+        (contentPlaceholderNode?.contains(target) && targetStyle.userSelect !== 'none')
       ) {
         const syntheticEvent = event || getSyntheticEvent(ev);
         selection.removeAllRanges();
         const rangeFromPoint = getRangeFromPoint(syntheticEvent);
         if (rangeFromPoint) {
-          selection.addRange(rangeFromPoint);
+          selection.addRange(rangeFromPoint.cloneRange());
           const range = selection.getRangeAt(0);
           selection.modify('move', 'backward', 'word');
           selection.modify('extend', 'forward', 'word');
@@ -38,13 +38,48 @@ const getWordSelection = (
           ) {
             const finalRange = selection.getRangeAt(0);
             const { left, right, top, bottom } = finalRange.getBoundingClientRect();
+            const { clientX, clientY } = syntheticEvent;
             if (
-              left - 1 <= syntheticEvent.clientX &&
-              right + 1 >= syntheticEvent.clientX &&
-              top - 1 <= syntheticEvent.clientY &&
-              bottom + 1 >= syntheticEvent.clientY
+              (left - 1 <= clientX &&
+                right + 1 >= clientX &&
+                top - 1 <= clientY &&
+                bottom + 1 >= clientY) ||
+              (state.layout === 'fixed' &&
+                targetStyle.cursor === 'text' &&
+                clientX > right &&
+                clientY < bottom &&
+                clientY > top)
             ) {
               res = finalRange;
+            } else if (
+              state.layout === 'fixed' &&
+              targetStyle.cursor === 'text' &&
+              clientX < left &&
+              clientY > bottom
+            ) {
+              selection.removeAllRanges();
+              selection.addRange(rangeFromPoint.cloneRange());
+              const newPointRange = selection.getRangeAt(0);
+              selection.modify('extend', 'forward', 'word');
+              if (
+                selection.toString().trim() &&
+                (force ||
+                  ((newPointRange.startContainer === target ||
+                    target.contains(newPointRange.startContainer)) &&
+                    (newPointRange.endContainer === target ||
+                      target.contains(newPointRange.endContainer))))
+              ) {
+                const fixedRange = selection.getRangeAt(0);
+                const fixedRect = fixedRange.getBoundingClientRect();
+                if (
+                  fixedRect.left - 1 <= clientX &&
+                  fixedRect.right + 1 >= clientX &&
+                  fixedRect.top - 1 <= clientY &&
+                  fixedRect.bottom + 1 >= clientY
+                ) {
+                  res = fixedRange;
+                }
+              }
             }
           }
         }
