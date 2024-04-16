25ca8922cf960ced1713e1076ecac49504ef7302
rai <rai@binpar.com>
2024-04-16 13:23:04 +0200

    added t3

---
+++ webpack.config.app-t3.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  entry: './dist/index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../read.garden-mobile-t3/sdk-viewer/js/'),
+    filename: 'read.garden-viewer.js',
+    clean: true,
+  },
+};
